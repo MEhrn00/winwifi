f06298ed2be41d231b3f2560a6239a0c10070a7c
Matt Ehrnschwender <matthewe2020@gmail.com>
2024-01-28 17:44:40 -0500

    Slight refactor of GUID

--- src/guid.rs
+++ src/guid.rs
@@ -1,66 +1,55 @@
-use std::{fmt::Display, marker::PhantomData};
+use std::fmt::Display;
 
 use windows::core::GUID;
 
 /// Reference to a Windows GUID with added functionality
 #[derive(Debug)]
 #[repr(transparent)]
-pub struct GuidRef<'a> {
-    guid: *const GUID,
-    _marker: PhantomData<&'a GUID>,
-}
+pub struct GuidRef<'a>(&'a GUID);
 
 impl<'a> GuidRef<'a> {
     /// # Safety
     ///
     /// Pointer validation is not done. Caller has to ensure that a valid pointer to a GUID is passed in
     pub unsafe fn from_ptr(guid: *const GUID) -> GuidRef<'a> {
-        GuidRef {
-            guid,
-            _marker: PhantomData,
-        }
+        GuidRef(&*guid)
     }
 
     pub(crate) fn as_ptr(&self) -> *const GUID {
-        self.guid
+        self.0
     }
 }
 
 impl<'a> std::cmp::PartialEq<GUID> for GuidRef<'a> {
     fn eq(&self, other: &GUID) -> bool {
-        unsafe { &(*self.guid) == other }
+        self.0 == other
     }
 }
 
 impl<'a> From<&'a GUID> for GuidRef<'a> {
     fn from(value: &'a GUID) -> Self {
-        GuidRef {
-            guid: value,
-            _marker: PhantomData,
-        }
+        GuidRef(value)
     }
 }
 
 impl Display for GuidRef<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        unsafe {
-            let mut clock_seq: [u8; 2] = Default::default();
-            let mut node: [u8; 8] = Default::default();
+        let mut clock_seq: [u8; 2] = Default::default();
+        let mut node: [u8; 8] = Default::default();
 
-            // Copy out the clock_seq and node from the guid without unnecessarily allocating a copy of it
-            clock_seq.clone_from_slice(&(*self.guid).data4[..2]);
-            node[2..].clone_from_slice(&(*self.guid).data4[2..]);
+        // Copy out the clock_seq and node from the guid without unnecessarily allocating a copy of it
+        clock_seq.clone_from_slice(&self.0.data4[..2]);
+        node[2..].clone_from_slice(&self.0.data4[2..]);
 
-            write!(
-                f,
-                "{:x}-{:x}-{:x}-{:x}-{:x}",
-                (*self.guid).data1,
-                (*self.guid).data2,
-                (*self.guid).data3,
-                u16::from_be_bytes(clock_seq),
-                u64::from_be_bytes(node),
-            )
-        }
+        write!(
+            f,
+            "{:x}-{:x}-{:x}-{:x}-{:x}",
+            self.0.data1,
+            self.0.data2,
+            self.0.data3,
+            u16::from_be_bytes(clock_seq),
+            u64::from_be_bytes(node),
+        )
     }
 }
 
